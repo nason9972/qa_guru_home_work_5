8e0990e4a7542735b9d2403ea0c59ea639a3b6e8
Ivan Nasonov <nason9972@rambler.ru>
2025-11-05 21:53:01 +0300

    Исправление

--- Test/test_registration.py
+++ Test/test_registration.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selene import browser, have, be
 from selenium import webdriver
@@ -7,7 +9,7 @@ from selenium import webdriver
 def config_browser():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver_options = webdriver.ChromeOptions()
-    browser.config.driver_options.add_argument('--headless=new')
+    #browser.config.driver_options.add_argument('--headless=new')
     browser.config.driver_options.add_argument('--window-size=1920,1080')
     browser.config.timeout = 8
     yield
@@ -17,8 +19,7 @@ def config_browser():
 def test_registration_form(config_browser):
     browser.open('/automation-practice-form')
 
-    # проверка Title
-    #browser.should(have.title('DEMOQA'))
+
 
     # заполнение полей и проверки
     browser.element('#firstName').should(be.visible).should(be.clickable).type('Иван')
@@ -35,6 +36,7 @@ def test_registration_form(config_browser):
     browser.element('.react-datepicker__day.react-datepicker__day--025').click()
     browser.element('#subjectsInput').type('Math').press_enter()
     browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture').set_value(os.path.abspath('../test_data/test_image.png'))
     browser.element('#currentAddress').should(be.visible).should(be.clickable).type('Moscow, st.Lenina, 23')
     browser.element('#state').click()
     browser.element('#react-select-3-input').type('NCR').press_enter()
@@ -43,15 +45,19 @@ def test_registration_form(config_browser):
     browser.element('#submit').should(be.visible).should(be.clickable).click()
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     # проверка таблички
-    browser.element('.table').should(have.text('Иван Иванов'))
-    browser.element('.table').should(have.text('ivanov@example.com'))
-    browser.element('.table').should(have.text('Male'))
-    browser.element('.table').should(have.text('7929100500'))
-    browser.element('.table').should(have.text('25 November,1986'))
-    browser.element('.table').should(have.text('Maths'))
-    browser.element('.table').should(have.text('Sports'))
-    browser.element('.table').should(have.text('Moscow, st.Lenina, 23'))
-    browser.element('.table').should(have.text('NCR Delhi'))
+    browser.all('.table td').should(have.exact_texts(
+        'Student Name', 'Иван Иванов',
+        'Student Email', 'ivanov@example.com',
+        'Gender', 'Male',
+        'Mobile', '7929100500',
+        'Date of Birth', '25 November,1986',
+        'Subjects', 'Maths',
+        'Hobbies', 'Sports',
+        'Picture', 'test_image.png',
+        'Address', 'Moscow, st.Lenina, 23',
+        'State and City', 'NCR Delhi'
+    ))
+
 
 
 
